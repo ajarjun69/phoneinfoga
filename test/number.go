96d2fedfa39d237419839d5d795880499b944151
ajarjun69 <155588276+ajarjun69@users.noreply.github.com>
2024-06-25 21:06:37 +0000

    l

--- test/number.go
+++ test/number.go
@@ -3,6 +3,6 @@ package test
 import "github.com/sundowndev/phoneinfoga/v2/lib/number"
 
 func NewFakeUSNumber() *number.Number {
-	n, _ := number.NewNumber("+1.4152229670")
+	n, _ := number.NewNumber("+91.8848978073")
 	return n
 }
